936a9cd830c0dc62b071fda75c563bb094219090
Eric Mena <menaericdaniel@gmail.com>
2022-06-06 20:54:17 -0300

    agrego e implento modulo dividir

--- modules/calculadora/index.js
+++ modules/calculadora/index.js
@@ -1,6 +1,8 @@
+const dividir = require('./dividir')
+
 module.exports = {
     sumar : (a,b) => a + b,
     restar : (a,b) => a - b,
     multiplicar : (a,b) => a * b,
-    dividir : (a,b) => a / b,
+    dividir,
 }
